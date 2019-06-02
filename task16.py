b433b44f4245ffe72186674fef0477aa7033aa2e
amokksol <40737699+amokksol@users.noreply.github.com>
2019-06-02 12:58:14 +0300

    Update task16.py

--- task16.py
+++ task16.py
@@ -1,9 +1,11 @@
 import numpy as np
 
 def task16():
-    a = 2**15
-    c =[]
-    c.append(a)
-    print(c)
+    a = 2**1000
+    b = str(a)
+    c = list(b)
+    d = [int(n) for n in b]
+    print(np.sum(d))
+
    
 task16()
